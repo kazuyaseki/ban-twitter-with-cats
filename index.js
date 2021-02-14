b311a210322e9d3f935faa493aa79100fdcd75e1
kazuyaseki <track613armory@gmail.com>
2021-02-15 00:16:07 +0900

    show local cat image instead of kittenplaceholder

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const KEY = "JHGEJSADKLGKLSFDSFFJ"
 
+const CAT_IMAGES_COUNT = 20
+
 window.onload = function() {
   var body = document.getElementsByTagName('body');
 
   chrome.storage.sync.get([KEY], function(result) {
-    const enabled = !!result[KEY]
+    // judges as enabled when the value is empty since it must be right after installing the extension
+    const enabled = result[KEY] || result[KEY] === undefined
+
+    const catImageIndex= Math.floor(Math.random() * (CAT_IMAGES_COUNT - 1)) + 1  
+    const path = chrome.runtime.getURL(`/cat_images/${catImageIndex}.jpeg`)
 
     if(enabled) {
       body[0].innerHTML = `
       <div style="width: 100vw; height: 100vh; display: flex; justify-content:center; align-items: center">
-        <img src="http://placekitten.com/480/480" alt="cat" />
-        ${enabled}
+        <img src="${path}" alt="cat" />
       </div>`;
     }
   });
