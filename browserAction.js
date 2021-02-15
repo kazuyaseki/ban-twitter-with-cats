81e0cfed01feafa04cbfa6c1195b71a3e46b024a
kazuyaseki <track613armory@gmail.com>
2021-02-15 11:53:33 +0900

    modified to toggle status without popup

--- browserAction.js
+++ browserAction.js
@@ -1,12 +1,12 @@
 const KEY = "JHGEJSADKLGKLSFDSFFJ"
 
-window.onload = function() {
+chrome.browserAction.onClicked.addListener(function() {
   chrome.storage.sync.get([KEY], function(result) {
     const enabled = !!result[KEY]
 
     const iconPath = enabled ? "cat_disabled.png" : "cat_enabled.png"
     chrome.browserAction.setIcon({path: iconPath}, () => {
-      chrome.storage.sync.set({[KEY]: !enabled}, () => window.close());
+      chrome.storage.sync.set({[KEY]: !enabled});
     });
   });
-};
+});
